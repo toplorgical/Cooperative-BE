ae548ab2ffeb94c867d864eda7f0ef4f0f92a689
EGOBA KELVIN <egobakelvin@gmail.com>
2024-07-08 16:21:45 +0100

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, world!');
+});
+
+
+
+export default app
+
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import http from 'http'
+import app from './app';
+
+const server = http.createServer(app)
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+  
