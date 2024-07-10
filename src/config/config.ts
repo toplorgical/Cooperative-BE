c3b23447d9a9d39f1f6c7788d16b0603faf2304b
Rilwan Aribidesi <aribidesirilwan1477@gmail.com>
2024-07-10 11:24:37 +0100

    added messaging service

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,10 @@ dotenv.config();
 const config = {
   PORT: process.env.PORT || 8000,
   JWT_KEY: process.env.JWT_KEY as string,
+  TERMII: {
+    API_KEY: process.env.TERMII_API_KEY as string,
+    URL: "https://v3.api.termii.com/api/sms/send",
+  },
   DATABASE: {
     URL: process.env.DATABASE_URL,
     PORT: process.env.DATABASE_PORT,
