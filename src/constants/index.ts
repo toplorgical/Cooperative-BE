2e686346e524341e372580eb57c1450af328f67c
EGOBA KELVIN <egobakelvin@gmail.com>
2024-07-10 12:27:31 +0100

    response message added

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,3 +4,9 @@ export const RESPONSE ={
     USER_NOT_FOUND : "The requsted user could not be found",
     USER_EXIST : "The user already exist"
 }
+
+
+
+export const smsResponse = {
+    message : `Your verification code is otp Use this code to verify your account`
+}
